b39aa5d75ea3b93a853b2e1e84822902a6d0bc25
Santosh <santusb427@gmail.com>
2022-01-29 00:19:31 +0530

    CalculateChange

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
-    if(billAmount.value > 0) {
+    if(billAmount.value > 0 ) {
         if(cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
@@ -21,15 +22,19 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-    for (let i=0; i<availableNotes.length; i++) {
-        const numberOFNOtes = math.trunc(amountToBeReturned/availableNotes[0]);
-        amountToBeReturned %= availableNotes[0];
-
+    //
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+            );
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+        
     }
     
 }
 
-function hideMessage(){
+function hideMessage() {
     message.style.display = "none";
 }
 
